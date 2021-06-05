3f0832565371794f379b8783e3fe7391febfabac
Kausar Akther <kausar.akther@gmail.com>
2021-06-05 11:02:41 -0400

    heroku testing

---
+++ server.js
@@ -0,0 +1,11 @@
+//DEPENDENCIES
+const express = require('express')
+
+
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log('listening on port ', PORT)
+})
